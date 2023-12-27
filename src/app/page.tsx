b70492a801fba194684c4d73746237d115ba6350
Dmitrii Deganov <dmdeganov@yandex.ru>
2023-12-27 16:53:27 +0300

    test build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,30 @@
 'use client';
 import React, {useEffect, useRef} from 'react';
 import Header from '@/components/Header';
-import {useInView} from '@react-spring/core';
 import PrinciplesSlider from '@/components/PrinciplesSlider';
 import OutlinedButton from '@/components/OutlinedButton';
-// import ThreeCanvas from '@/components/ThreeCanvas';
-// import IPhoneScene from '@/app/test/IphoneScene';
-// import {Canvas} from "@react-three/fiber";
+
+
 
 const Page = () => {
   const sliderRef = useRef<HTMLDivElement>(null);
-  const [principlesSectionRef, inView] = useInView({amount: 0.9});
-  console.log({inView});
-
   const scrollToNextSlide = () => {
     if (!sliderRef.current) return;
     sliderRef.current.scrollBy(0, 100);
   };
+
 
   return (
     <>
       <Header />
       <main>
         {/*<div className="slider-wrapper">*/}
-        <div className="slider" style={{overflowY: inView ? 'hidden' : 'scroll'}} ref={sliderRef}>
+        <div className="slider" ref={sliderRef}>
           <div className="slider__slide">
             1<OutlinedButton onClick={scrollToNextSlide}>click</OutlinedButton>
           </div>
           <div className="slider__slide">2</div>
-          <div className="slider__slide slider__slide--horizontal" ref={principlesSectionRef}>
+          <div className="slider__slide slider__slide--horizontal">
             <PrinciplesSlider scrollParentSlider={(x: number, y: number) => sliderRef.current?.scrollBy(x, y)} />
           </div>
           <div className="slider__slide">4</div>
--- src/components/PrinciplesSlider.tsx
+++ src/components/PrinciplesSlider.tsx
@@ -4,27 +4,30 @@ import OutlinedButton from '@/components/OutlinedButton';
 const PrinciplesSlider = ({scrollParentSlider}: {scrollParentSlider?: (x: number, y: number) => void}) => {
   const ref = useRef<HTMLDivElement>(null);
 
-  function eventWheel(e: React.WheelEvent<HTMLDivElement>) {
+  function eventWheel(e: WheelEvent) {
     if (!ref.current) return;
-    const leftEdgeReached = ref.current.scrollLeft === 0;
-    const rightEdgeReached = ref.current.scrollLeft + ref.current.clientWidth >= ref.current.scrollWidth - 10;
-    if (leftEdgeReached && e.deltaY < 0) {
-      scrollParentSlider!(0, -100);
+    const needToScrollUp = ref.current.scrollLeft === 0 && e.deltaY < 0;
+    const needToScrollDown =
+      ref.current.scrollLeft + ref.current.clientWidth >= ref.current.scrollWidth - 10 && e.deltaY > 0;
+    console.log(needToScrollUp, needToScrollDown);
+    if (needToScrollUp || needToScrollDown) {
+      return;
+    } else {
+      e.stopPropagation();
+      e.preventDefault();
+      ref.current.scrollBy(e.deltaY || e.deltaX, 0);
     }
-    if (rightEdgeReached && e.deltaY > 0) {
-      scrollParentSlider!(0, 100);
-      ref.current!.scrollTo(0, 0);
-    }
-    ref.current.scrollBy(e.deltaY, 0);
   }
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    ref.current!.addEventListener('wheel', eventWheel);
+  }, []);
 
   return (
     <div className="principles">
       <OutlinedButton onClick={() => scrollParentSlider!(0, 100)}>click</OutlinedButton>
       <div className="principles-slider-wrapper">
-        <div className="principles-slider" ref={ref} onWheel={eventWheel}>
+        <div className="principles-slider" ref={ref}>
           <div className="principles-slider__slide">1</div>
           <div className="principles-slider__slide">2</div>
           <div className="principles-slider__slide">3</div>
