097a8045e93cfef400ce0d100155da4069080872
Dmitrii Deganov <dmdeganov@yandex.ru>
2024-01-22 15:37:57 +0300

    test

--- src/components/IphoneMotion.tsx
+++ src/components/IphoneMotion.tsx
@@ -3,45 +3,52 @@ import {motion} from 'framer-motion';
 import {Button} from '@mui/material';
 
 const frameCount = 120;
-const getFrameSrc = (index: number) => `static/iphone/${index.toString().padStart(4, '0')}-min.png`;
+const getFrameSrc = (index: number) => `iphone/${index.toString().padStart(4, '0')}-min.png`;
 
 const preloadImages = () => {
   for (let i = 1; i < frameCount; i++) {
     const img = new Image();
+    img.width = 0;
+    img.height = 0;
+
+    // Add the id attribute to the <img> element
+    img.id = `iphone-${i}`;
     img.src = getFrameSrc(i);
+    document.body.appendChild(img);
   }
 };
-const imageWidth = 360;
-const imageHeight = 326;
-const imageSize = 1920;
+
+let counter = 0;
+
 const animationDuration = 2000;
 
 const fps = animationDuration / frameCount;
 
 const IphoneMotion = ({currentSlide}: {currentSlide: number}) => {
-  const imgRef = useRef<HTMLDivElement>(null);
+  const imgRef = useRef<HTMLImageElement>(null);
 
   const prevSlideRef = useRef(0);
   const getAnimatedStyles = () => {
     if (currentSlide === 0) {
-      return {scale: 1.4, x: -50, rotateZ: '10deg', opacity: 1};
+      return {scale: 1.4, x: -250, opacity: 1};
     }
     if (currentSlide === 1) {
       return {
         scale: 1.4,
-        x: -200,
+        x: -400,
         y: 100,
         rotateZ: '0deg',
         opacity: 1,
       };
     }
-    return {scale: 1, x: -200, y: 100, rotateZ: '0deg', opacity: 0};
+    return {scale: 1, x: -200, y: 100, opacity: 0};
   };
 
   const shouldPreventChangingVideoSrcRef = useRef(false);
 
   useEffect(() => {
-    imgRef.current!.style.backgroundImage = `url(${getFrameSrc(1)})`;
+    const img = imgRef.current!;
+    img.src = getFrameSrc(1);
   }, []);
 
   const animationStartedAtRef = useRef(0);
@@ -54,19 +61,18 @@ const IphoneMotion = ({currentSlide}: {currentSlide: number}) => {
         const time = Date.now();
         const timePassed = time - animationStartedAtRef.current;
         const actualFrame = Math.ceil(timePassed / fps);
-        // console.log(timePassed, actualFrame);
         if (actualFrame <= 119) {
           if (actualFrame === currentFrameRef.current) {
             drawFrame();
             return;
           }
           currentFrameRef.current = actualFrame;
-          imgRef.current!.style.backgroundImage = imgRef.current!.style.backgroundImage = `url(${getFrameSrc(
-            actualFrame,
-          )})`;
-
+          imgRef.current!.src = getFrameSrc(actualFrame);
           drawFrame();
+          counter++;
         } else {
+          console.log(counter);
+          counter = 0;
           return;
         }
       });
@@ -93,10 +99,11 @@ const IphoneMotion = ({currentSlide}: {currentSlide: number}) => {
       >
         start
       </Button>
-      <motion.div
-        style={{width: '672.5px', aspectRatio: 1}}
+      <motion.img
+        height="100%"
+        style={{width: '672.5px', aspectRatio: 1, display: 'block'}}
         ref={imgRef}
-        // animate={getAnimatedStyles()}
+        animate={getAnimatedStyles()}
         // initial={{opacity: 0}}
         // transition={{duration: 1.4, opacity: {duration: 0.2}}}
         className="iphone-motion"
